3e58c04b5fe2739ba846da7e71841cac358e1f58
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-08-07 12:40:25 +0900

    <추가>: <스프링4 EL수정하다>
    기존 : "${list } "
    수정 : "${list }"
    괄호 뒤에 공란있으면 찾지못함.

--- Spring04_Mybatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
+++ Spring04_Mybatis/src/main/java/com/gura/spring04/member/controller/MemberController.java
@@ -1,5 +1,7 @@
 package com.gura.spring04.member.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +13,20 @@ import com.gura.spring04.member.dto.MemberDto;
 @Controller
 public class MemberController {
 
-	
-	
+	//의존개체를 주입 받는다.
+	//빈 컨테이너에서 mebmerdao타입이 있으면 dao에 넣어달라는 의미이다.
 	@Autowired
 	private MemberDao dao;
 	
 	
-	//회원정보 추가 요청 처리
-	@RequestMapping("/member/insert")
-	public ModelAndView insert(MemberDto dto) {
-		
-		
-		return null;
+	@RequestMapping("/member/list")
+	public ModelAndView list(ModelAndView mView) {
+		//회원목록을 얻어온다.
+		List<MemberDto> list = dao.getList();
+		//model을 담는다.
+		mView.addObject("list",list);
+		//view 페이지 정보를 담고
+		mView.setViewName("member/list");
+		return mView; 
 	}
 }
--- Spring04_Mybatis/src/main/webapp/WEB-INF/views/home.jsp
+++ Spring04_Mybatis/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <%@ page session="false" %>
 <html>
@@ -5,10 +8,12 @@
 	<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+	<h1>안녕하세요 스프링 페이지입니다.</h1>
+	
+	<ul>
+		<li><a href="member/list.do">이동하세요</a></li>
+	</ul>
 </body>
 </html>
+
+
