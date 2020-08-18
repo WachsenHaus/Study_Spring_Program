e6e40be549aad61900a770f1c65b780b057296c0
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-08-18 10:13:28 +0900

    <추가>: <댓글기능을위한 sql문 추가>

---
+++ Spring05_final/src/main/webapp/WEB-INF/table.sql
@@ -0,0 +1,96 @@
+SELECT NUM,WRITER,TITLE,
+--lead는 다음 번호를 가져온다. prev는 최신글을 의미한다.
+--글은 앞에다 붙이기 떄문?
+LEAD(num,1,0) OVER (ORDER BY num DESC) AS prevNum 
+FROM BOARD_CAFE
+ORDER BY NUM DESC;
+
+SELECT NUM,WRITER,TITLE,
+LEAD(num,1,0) OVER (ORDER BY num DESC) AS prevNum,
+LAG(num,1,0) OVER (ORDER BY num DESC) AS nextNum,
+FROM BOARD_CAFE
+ORDER BY NUM DESC;
+
+
+
+CREATE TABLE board_cafe(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100) NOT NULL,
+	title VARCHAR2(100) NOT NULL,
+	content CLOB,
+	viewCount NUMBER, --조회수
+	regdate DATE
+);
+
+create SEQUENCE board_cafe_seq;
+
+
+create table board_cafe_comment(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100),
+	content VARCHAR2(500),
+	target_id VARCHAR2(100),
+	ref_group NUMBER,
+	comment_group NUMBER,
+	deleted CHAR(3) DEFAULT 'no',
+	regdate DATE
+);
+
+create sequence board_cafe_comment_seq;
+
+
+
+
+
+SELECT * FROM (SELECT result1.*, ROWNUM as rnum
+	FROM ( SELECT num,writer,title,orgFileName, fileSize,regdate FROM board_file
+		   WHERE title LIKE '%' || ? || '%' OR content LIKE '%' || ? || '%'
+		   ORDER BY num DESC ) result1)
+		--Like '%안녕%' 안녕이 포함,
+		--Like '안녕%' 안녕으로 시작,
+		--Like 안녕으로 검색한다.
+		--Like '%' || '안녕' || '%'
+		--Like 작성자 검사냐 내용검사냐
+WHERE rnum BETWEEN ? and ?
+
+
+SELECT * FROM board_file ORDER BY num DESC;
+
+CREATE TABLE board_gallery(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100),
+	caption VARCHAR2(200),
+	imagePath VARCHAR2(100),
+	regdate DATE
+);
+CREATE SEQUENCE board_gallery_seq;
+
+
+CREATE TABLE board_file(
+	num NUMBER PRIMARY KEY,
+	writer VARCHAR2(100) NOT NULL,
+	title VARCHAR2(100) NOT NULL,
+	orgFileName VARCHAR2(100) NOT NULL, --원본 파일명
+	saveFileName VARCHAR2(100) NOT NULL, --서버에 실제로 저장된 파일명
+	fileSize NUMBER NOT NULL,
+	regdate date
+);
+
+CREATE SEQUENCE board_file_seq;
+
+
+
+-- 사용자(회원) 정보를 저장할 테이블
+CREATE TABLE users(
+	id VARCHAR2(100) PRIMARY KEY,
+	pwd VARCHAR2(100) NOT NULL,
+	email VARCHAR2(100),
+	profile VARCHAR2(100), -- 프로필 이미지 경로를 저장할 칼럼
+	regdate DATE
+);
+
+
+create table company(
+	num number primary key,
+	
+)
